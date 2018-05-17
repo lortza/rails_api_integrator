4efe600f8df6c04619a70c1ca2aaf6d8e3257d14
anne <richardson.ae@gmail.com>
2018-05-17 16:45:23 -0500

    Adds major_cities into API weather output

--- app/models/area_weather_report.rb
+++ app/models/area_weather_report.rb
@@ -3,7 +3,8 @@ class AreaWeatherReportBase
   def self.get_report_data(state, city)
     weather_report = {
       current_city: Weather.get_weather_data(state, city),
-      nearby_cities: nearby_city_weather(state, city)
+      nearby_cities: city_weather(state, city, 'nearby'),
+      major_cities: city_weather(state, city, 'major')
     }
 
     self.new(weather_report)
@@ -11,22 +12,27 @@ class AreaWeatherReportBase
 
   private
 
-  def self.nearby_city_weather(state, city)
-    nearby_cities(state, city).map { |nc| Weather.get_weather_data(nc[:state], nc[:city]) }.compact[0..2]
+  def self.city_weather(state, city, type)
+    send("#{type}_cities", state, city).map { |nc| Weather.get_weather_data(nc[:state], nc[:city]) }.compact[0..2]
   end
 
   def self.nearby_cities(state, city)
     @nearby_cities = GeoLocation.get_nearby_cities(state, city)
   end
+
+  def self.major_cities(state, city)
+    @major_cities = GeoLocation.get_major_cities(state, city)
+  end
 end
 
 class AreaWeatherReport < AreaWeatherReportBase
-  attr_reader :nearby_cities
+  attr_reader :nearby_cities, :major_cities
   # Delegate moves all of the current_city info out into the AreaWeatherReport object
   delegate :state, :city, :local_time, :description, :temperature, to: :@current_city
 
   def initialize(attrs)
     @current_city = attrs[:current_city]
     @nearby_cities = attrs[:nearby_cities]
+    @major_cities = attrs[:major_cities]
   end
 end
--- app/models/geo_location.rb
+++ app/models/geo_location.rb
@@ -7,23 +7,6 @@ class GeoLocation
   BASE_URL = "https://wft-geo-db.p.mashape.com/v1/geo"
   COUNTRY_CODE = 'US'
 
-  def self.get_nearby_cities(state, city)
-    puts "Getting Nearby Cities for #{city}, #{state}"
-
-    cities_response = self.get_major_cities(state, city)
-    # cities_response = self.get_closest_cities(state, city)
-
-    cities_response.map do |c|
-      {
-        city: c['city'],
-        state: c['regionCode'],
-        id: c['id']
-      }
-    end
-  end
-
-
-  private
 
   def self.get_major_cities(state, city)
     puts "Getting Major Cities in #{state}"
@@ -36,51 +19,68 @@ class GeoLocation
           "X-Mashape-Host" => ENV['GEO_API_HOST']
         })
     cities_excluding_current_city = cities_response['data'].select {|c| c['city'] != city }
-    cities_excluding_current_city.each {|c| c['regionCode'] = state}
+
+    cities_excluding_current_city.map do |c|
+      {
+        city: c['city'],
+        state: state,
+        id: c['id']
+      }
+    end
   end
 
-  # def self.get_closest_cities(state, city)
-  #   puts "Getting cities closest to #{city}, #{state}"
 
-  #   qty_cities = 8
-  #   mile_radius = 100
+  def self.get_nearby_cities(state, city)
+    puts "Getting Nearby Cities for #{city}, #{state}"
 
-  #   city_id = self.get_current_city_id(state, city)
+    qty_cities = 8
+    mile_radius = 100
 
-  #   built_url = "#{BASE_URL}/cities/#{city_id}/nearbyCities?distanceunit=mi&limit=#{qty_cities}&radius=#{mile_radius}"
+    city_id = self.get_current_city_id(state, city)
 
-  #   cities_response = HTTParty.get(built_url, headers: {
-  #       "X-Mashape-Key" => ENV['GEO_API_KEY'],
-  #       "X-Mashape-Host" => ENV['GEO_API_HOST']
-  #     })
-  #   cities_response['data']
-  # end
+    built_url = "#{BASE_URL}/cities/#{city_id}/nearbyCities?distanceunit=mi&limit=#{qty_cities}&radius=#{mile_radius}"
 
+    cities_response = HTTParty.get(built_url, headers: {
+        "X-Mashape-Key" => ENV['GEO_API_KEY'],
+        "X-Mashape-Host" => ENV['GEO_API_HOST']
+      })
 
-  # def self.get_current_city_id(state, city)
-  #   cities_response = HTTParty.get(
-  #     "#{BASE_URL}/cities?namePrefix=#{city}&countryCode=#{COUNTRY_CODE}", headers: {
-  #         "X-Mashape-Key" => ENV['GEO_API_KEY'],
-  #         "X-Mashape-Host" => ENV['GEO_API_HOST']
-  #       })
-  #   us_cities = cities_response['data'].select {|c| c['countryCode'] == COUNTRY_CODE }
-  #   city_in_desired_state = us_cities.select {|c| c['regionCode'] == state }[0]
-
-  #   if city_in_desired_state
-  #     city_in_desired_state['id']
-  #   else
-  #     self.get_biggest_city(state)['id']
-  #   end
-  # end
+    cities_response['data'].map do |c|
+      {
+        city: c['city'],
+        state: c['regionCode'],
+        id: c['id']
+      }
+    end
+  end
 
 
-  # def self.get_biggest_city(state)
-  #   cities_response = HTTParty.get(
-  #     "#{BASE_URL}/countries/#{COUNTRY_CODE}/regions/#{state}/cities?sort=-population&offset=0&limit=1", headers: {
-  #         "X-Mashape-Key" => ENV['GEO_API_KEY'],
-  #         "X-Mashape-Host" => ENV['GEO_API_HOST']
-  #       })
-  #   cities_response['data'].first
-  # end
+  private
+
+  def self.get_current_city_id(state, city)
+    cities_response = HTTParty.get(
+      "#{BASE_URL}/cities?namePrefix=#{city}&countryCode=#{COUNTRY_CODE}", headers: {
+          "X-Mashape-Key" => ENV['GEO_API_KEY'],
+          "X-Mashape-Host" => ENV['GEO_API_HOST']
+        })
+    us_cities = cities_response['data'].select {|c| c['countryCode'] == COUNTRY_CODE }
+    city_in_desired_state = us_cities.select {|c| c['regionCode'] == state }[0]
+
+    if city_in_desired_state
+      city_in_desired_state['id']
+    else
+      self.get_biggest_city(state)['id']
+    end
+  end
+
+
+  def self.get_biggest_city(state)
+    cities_response = HTTParty.get(
+      "#{BASE_URL}/countries/#{COUNTRY_CODE}/regions/#{state}/cities?sort=-population&offset=0&limit=1", headers: {
+          "X-Mashape-Key" => ENV['GEO_API_KEY'],
+          "X-Mashape-Host" => ENV['GEO_API_HOST']
+        })
+    cities_response['data'].first
+  end
 
 end
